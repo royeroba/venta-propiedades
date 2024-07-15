4912933df77041617349dc55bc4959b2ac3892b3
royerobadev <royeroba2007@gmail.com>
2024-07-15 11:16:15 -0500

    change language to english

--- src/validation/propiedadSchema.js
+++ src/validation/propiedadSchema.js
@@ -1,38 +1,38 @@
 export const validationSchema = {
-    titulo (value) {
-      if (value?.length >= 6 ) return true
-      return 'El titulo de la propiedad es obligatorio o muy corto'
-    },
-    precio (value) {
-      /*
-          ^ - Representa el inicio de la cadena.
-          [0-9]+ - Representa cualquier número del 0 al 9. El + indica que debe haber al menos un dígito, pero puede haber más.
-          $ - Representa el final de la cadena.
-      */
-      if (/^[0-9]+$/.test(value)) return true
-      return 'El Precio solo deben ser números'
-    },
-    habitaciones (value) {
-      if (value) return true
-      return 'Selecciona una Cantidad'
-    },
-    wc (value) {
-      if (value) return true
-      return 'Selecciona una Cantidad'
-    },
-    estacionamiento (value) {
-      if (value) return true
-      return 'Selecciona una Cantidad'
-    },
-    descripcion (value) {
-      if (value) return true
-      return 'Agrega una Descripción'
-    }
-} 
-  
+  titulo(value) {
+    if (value?.length >= 6) return true
+    return 'Title to the property is mandatory or too short'
+  },
+  precio(value) {
+    /*
+        ^ - Representa el inicio de la cadena.
+        [0-9]+ - Representa cualquier número del 0 al 9. El + indica que debe haber al menos un dígito, pero puede haber más.
+        $ - Representa el final de la cadena.
+    */
+    if (/^[0-9]+$/.test(value)) return true
+    return 'Price should only be numbers'
+  },
+  habitaciones(value) {
+    if (value) return true
+    return 'Select a Quantity'
+  },
+  wc(value) {
+    if (value) return true
+    return 'Select a Quantity'
+  },
+  estacionamiento(value) {
+    if (value) return true
+    return 'Select a Quantity'
+  },
+  descripcion(value) {
+    if (value) return true
+    return 'Add a Description'
+  }
+}
+
 export const imageSchema = {
-  imagen (value) {
-      if (value) return true
-      return 'La Imagen es Obligatoria'
+  imagen(value) {
+    if (value) return true
+    return 'Image is required'
   },
 }
