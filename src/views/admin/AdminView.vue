8a0eb394b669f31a518902ec42faba954710e915
royerobadev <royeroba2007@gmail.com>
2024-07-15 13:12:01 -0500

    fix styles responsive to AdminView

--- src/views/admin/AdminView.vue
+++ src/views/admin/AdminView.vue
@@ -6,14 +6,16 @@ const { propiedadesCollection, deleteItem } = usePropiedades();
 
 <template>
   <h2 class="text-center text-h3 my-5 font-weight-bold">Admin Panel</h2>
-
-  <v-btn color="blue" variant="flat" :to="{ name: 'nueva-propiedad' }">
-    New Property
-  </v-btn>
+  <div class="btn-new-property">
+    <v-btn color="blue" variant="flat" :to="{ name: 'nueva-propiedad' }">
+      New Property
+    </v-btn>
+  </div>
 
   <v-card class="mx-auto mt-10" flat>
-    <v-list>
+    <v-list class="cards">
       <v-list-item
+        class="card-mobile"
         v-for="propiedad in propiedadesCollection"
         :key="propiedad.id"
       >
@@ -29,24 +31,67 @@ const { propiedadesCollection, deleteItem } = usePropiedades();
         </v-list-item-subtitle>
 
         <template v-slot:append>
-          <v-btn
-            color="info"
-            flat
-            class="mr-2"
-            :to="{ name: 'editar-propiedad', params: { id: propiedad.id } }"
-          >
-            Edit
-          </v-btn>
+          <div class="btn-card-container">
+            <v-btn
+              color="info"
+              flat
+              block
+              class="mr-2"
+              :to="{ name: 'editar-propiedad', params: { id: propiedad.id } }"
+            >
+              Edit
+            </v-btn>
 
-          <v-btn
-            color="red-darken-3"
-            flat
-            @click="deleteItem(propiedad.id, propiedad.imagen)"
-          >
-            Delete
-          </v-btn>
+            <v-btn
+              color="red-darken-3"
+              flat
+              block
+              @click="deleteItem(propiedad.id, propiedad.imagen)"
+            >
+              Delete
+            </v-btn>
+          </div>
         </template>
       </v-list-item>
     </v-list>
   </v-card>
 </template>
+
+<style scoped>
+@media (max-width: 600px) {
+  .btn-new-property {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+}
+
+.cards {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+
+@media (max-width: 600px) {
+  .card-mobile {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    align-items: flex-start;
+  }
+}
+
+.btn-card-container {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+}
+
+@media (max-width: 600px) {
+  .btn-card-container {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+}
+</style>
